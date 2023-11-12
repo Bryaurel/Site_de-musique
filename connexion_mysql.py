364b6b444b5e16a582c420009abbedd6bb98f9d1
Bryaurel <b.bakongobw@alustudent.com>
2023-11-13 01:05:24 +0200

    connexion

--- connexion_mysql.py
+++ connexion_mysql.py
@@ -15,3 +15,19 @@ if connexion.is_connected():
     print("Connexion réussie")
 else:
     print("Échec de la connexion")
+
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/inscription', methods=['POST'])
+def inscription():
+    nom = request.form['nom']
+    email = request.form['email']
+    mot_de_passe = request.form['motdepasse']
+    # Traitez ces données (insérez-les dans la base de données, etc.)
+    return "Données capturées : {} - {} - {}".format(nom, email, mot_de_passe)
+
+if __name__ == '__main__':
+    app.run()
