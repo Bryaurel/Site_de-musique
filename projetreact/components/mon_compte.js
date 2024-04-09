3fee0c6a97fcc4e1b59a2966dcf3cbb34e09857b
Bryaurel <b.bakongobw@alustudent.com>
2024-04-09 23:48:02 +0200

    change

---
+++ projetreact/components/mon_compte.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MonCompte() {
+  return (
+    <div>
+      <h2>Mon Compte</h2>
+      {/* Ajoutez le contenu de la page Mon Compte ici */}
+    </div>
+  );
+}
+
+export default MonCompte;
